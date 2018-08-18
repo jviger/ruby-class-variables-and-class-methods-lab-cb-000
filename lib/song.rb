6336e21d994d4b75516c535ff46bbb99f53e5e12
Jason Viger <jason.viger@gmail.com>
2018-08-18 10:46:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,17 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0 
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists.push[artist]
   end
   
+
   
   
   
